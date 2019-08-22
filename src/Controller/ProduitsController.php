218d4864b502c7246e29d83b13e1af4c2d51dc81
Cyril Belfy <cyril.belfy@outlook.fr>
2019-08-22 10:48:57 +0200

    Page Accueil

---
+++ src/Controller/ProduitsController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProduitsController extends AbstractController
+{
+
+/**
+ * @Route("/biens", name="produits")
+ * @return Response
+**/
+
+public function index(): Response
+{
+    return $this->render('produits/index.html.twig', [
+    'current_menu' => 'produits'
+    ]);
+}
+
+}
